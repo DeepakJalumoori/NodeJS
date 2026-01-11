157262f3074653c070b89a01a5d9049dd85cda14
Deepak <deepakjalmoori@gmail.com>
2026-01-11 06:24:47 +0530

    Exploring GET,POST,DELETE

---
+++ homework.md
@@ -0,0 +1,4 @@
+1.REST API
+2.HTTP Methods
+3.Servers and DNS
+4.Difference between PUT and PATCH
--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
 const app = express();
 
+
+app.get("/user",(req,res) => {
+  res.send({name: "Deepak", age: 24, city: "Bangalore"});
+});
+
+app.delete("/user", (req,res) => {
+  res.send("Delete Request Called");
+});
+
+app.post("/user", (req,res) => {
+  res.send("Data saved to the database successfully");
+});
+
 //Request Handler function
-app.use((req,res) => {
+app.use("/test",(req,res) => {
   res.send("Hello from the server side!!!");
 })
 
